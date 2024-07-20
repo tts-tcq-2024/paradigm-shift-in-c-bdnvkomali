a2faf24d6ec001a3e507ff9a931d9b25567430cd
bdnvkomali <bdnvkomali@gmail.com>
2024-07-20 19:03:58 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -7,18 +7,21 @@ int temperature_check(float temperature) {
 }
  
 int soc_check(float soc) {
-    return (soc >= 20 && soc <=84);
+    return (soc >= 20 && soc <=80);
 }
 int charge_rate_check(float chargeRate) {   
  return (chargeRate <= 0.8);
 
 }
 int battery_check(float temperature, float soc, float chargeRate){
-    if (temperature_check(temperature) && soc_check(soc) && charge_rate_check(chargeRate))
+    if {(temperature_check(temperature) && soc_check(soc) && charge_rate_check(chargeRate))
    printf("Battery is okay\n");
-  else 
+return 1:
+}
+else {
    printf("Battery is Not  okay\n");
-    }
+    return 0:
+}
 int main() {
   assert(battery_check(25, 70, 0.7) == true);
   assert(battery_check(50, 85, 0) == false);
